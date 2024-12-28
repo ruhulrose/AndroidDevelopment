625938492e0f93f33188e9b225badb76ef3c1c8a
Md. Ruhul Amin <ruhul.rose5153@gmail.com>
2024-12-28 23:54:31 +0600

    I learn about button
    Today I learn about button and its alignment in relative layout.

--- app/src/main/java/com/example/androiddevelopment/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.androiddevelopment;
 
+import static androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
